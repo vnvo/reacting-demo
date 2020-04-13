501a5f504cb96d5eb520a59a42b979020786de03
vnvo <vahid.negahdari@gmail.com>
2020-04-13 08:59:42 +0300

    fixed the error with JSX

--- src/App.js
+++ src/App.js
@@ -8,12 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There Keeper - Let's kick this thing in the Ass!
+          Hi There Keeper - Let's kick this thing in the Ass! - this time using feature branch
         </p>
       </header>
-    </div>
-    <div>
-      a small change to test git workflow by feature branch!
     </div>
   );
 }
